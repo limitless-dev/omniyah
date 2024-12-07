b137870f3f1e38ca8f93a253d9447a2fb45d1c9a
Limitless <abdulellah15@gmail.com>
2024-12-07 23:08:04 +0300

    feat: animated search bar in header

--- app/(app)/(protected)/home/index.tsx
+++ app/(app)/(protected)/home/index.tsx
@@ -1,9 +1,17 @@
+import { Icon } from '@roninoss/icons';
 import { ListRenderItemInfo } from '@shopify/flash-list';
-import { Stack } from 'expo-router';
-import React from 'react';
-import { Platform, SafeAreaView, ScrollView, View } from 'react-native';
+import * as React from 'react';
+import { Platform, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-controller';
+import Animated, {
+  FadeIn,
+  LinearTransition,
+  ZoomOut,
+} from 'react-native-reanimated';
 
+import { Button } from '~/components/nativewindui/Button';
 import { LargeTitleHeader } from '~/components/nativewindui/LargeTitleHeader';
+import { LargeTitleSearchBarRef } from '~/components/nativewindui/LargeTitleHeader/types';
 import {
   ESTIMATED_ITEM_HEIGHT,
   List,
@@ -11,37 +19,97 @@ import {
   ListItem,
   ListSectionHeader,
 } from '~/components/nativewindui/List';
+import { Text } from '~/components/nativewindui/Text';
 import { HorizontalScrollChips } from '~/components/ui/HorizontalScrollChips';
-import { Text } from '~/components/ui/text';
-
-const SCREEN_OPTIONS = {
-  // title: 'Feed',
-  headerTransparent: Platform.OS === 'ios',
-  // headerShown: true,
-  headerBlurEffect: 'systemMaterial',
-} as const;
+import { useColorScheme } from '~/lib/useColorScheme';
 
 export default function HomeScreen() {
+  const { colors } = useColorScheme();
+  const searchBarRef = React.useRef<LargeTitleSearchBarRef>(null);
+  const [materialPreset, setMaterialPreset] = React.useState<
+    'stack' | 'inline'
+  >('stack');
   return (
     <>
-      <LargeTitleHeader title="Feed" materialPreset="stack" />
-      <Stack.Screen options={SCREEN_OPTIONS} />
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <SafeAreaView className="flex-1 bg-background">
-          <View className="p-5">
-            <HorizontalScrollChips />
-          </View>
-          <View className="flex-1 pb-4">
-            <List
-              variant="insets"
-              data={DATA}
-              estimatedItemSize={ESTIMATED_ITEM_HEIGHT.titleOnly}
-              renderItem={renderItem}
-              keyExtractor={keyExtractor}
+      <LargeTitleHeader
+        title="Feed"
+        materialPreset={materialPreset}
+        rightView={() => (
+          <Button variant="plain" size="icon">
+            <Icon
+              size={24}
+              name="account-circle-outline"
+              color={colors.foreground}
             />
-          </View>
-        </SafeAreaView>
-      </ScrollView>
+          </Button>
+        )}
+        screen={{
+          contentStyle: { backgroundColor: colors.background },
+          title: 'Feed',
+        }}
+        searchBar={{
+          ref: searchBarRef,
+          onChangeText: (text) => {
+            console.log(text);
+          },
+          materialRightView() {
+            return (
+              <Animated.View entering={FadeIn} exiting={ZoomOut}>
+                <Button variant="plain" size="icon">
+                  <Icon
+                    size={24}
+                    name="cog-outline"
+                    color={colors.foreground}
+                  />
+                </Button>
+              </Animated.View>
+            );
+          },
+          content: (
+            <KeyboardAwareScrollView
+              className="ios:bg-background/95"
+              contentContainerClassName="flex-1"
+              keyboardShouldPersistTaps="always"
+            >
+              <View className="flex-1 items-center justify-center">
+                <Text>Search bar content</Text>
+              </View>
+            </KeyboardAwareScrollView>
+          ),
+        }}
+      />
+      <Animated.ScrollView
+        layout={LinearTransition}
+        contentInsetAdjustmentBehavior="automatic"
+        contentContainerClassName="py-2"
+      >
+        <View className="px-4 pb-2">
+          <HorizontalScrollChips />
+        </View>
+        <List
+          /* the variant "insets" will add padding px-4 */
+          variant="insets"
+          data={DATA}
+          estimatedItemSize={ESTIMATED_ITEM_HEIGHT.titleOnly}
+          renderItem={renderItem}
+          keyExtractor={keyExtractor}
+        />
+
+        {Platform.OS !== 'ios' && (
+          <Button
+            variant={materialPreset === 'inline' ? 'secondary' : 'tonal'}
+            onPress={() => {
+              setMaterialPreset((prev) =>
+                prev === 'inline' ? 'stack' : 'inline'
+              );
+            }}
+          >
+            <Text>
+              Switch to {materialPreset === 'inline' ? 'stack' : 'inline'}
+            </Text>
+          </Button>
+        )}
+      </Animated.ScrollView>
     </>
   );
 }
@@ -74,71 +142,24 @@ function keyExtractor(
   return typeof item === 'string' ? item : item.id;
 }
 
-const DATA = [
-  'Header',
-  {
-    id: '1',
-    title: 'Hello',
-    subTitle: 'World',
-  },
-  {
-    id: '2',
-    title: 'Hello',
-    subTitle: 'World',
-  },
+function generateRandomData(count: number) {
+  const sections = Math.ceil(count / 4);
+  const data: (ListDataItem & { id: string })[] = [];
+  for (let i = 0; i < sections; i++) {
+    // @ts-expect-error: string is not assignable to ListDataItem. however, it's used for the section header
+    data.push(`Header ${i + 1}`);
 
-  {
-    id: '3',
-    title: 'Hello',
-    subTitle: 'World',
-  },
-  {
-    id: '4',
-    title: 'Hello',
-    subTitle: 'World',
-  },
-  'Header 2',
-  {
-    id: '5',
-    title: 'Hello',
-    subTitle: 'World',
-  },
-  {
-    id: '6',
-    title: 'Hello',
-    subTitle: 'World',
-  },
+    for (let j = 0; j < 4; j++) {
+      const id = (i * 4 + j + 1).toString();
+      data.push({
+        id,
+        title: `Random Title ${Math.floor(Math.random() * 1000)}`,
+        subTitle: `Random Subtitle ${Math.floor(Math.random() * 1000)}`,
+      });
+    }
+  }
 
-  {
-    id: '7',
-    title: 'Hello',
-    subTitle: 'World',
-  },
-  {
-    id: '8',
-    title: 'Hello',
-    subTitle: 'World',
-  },
-  'Header 3',
-  {
-    id: '9',
-    title: 'Hello',
-    subTitle: 'World',
-  },
-  {
-    id: '10',
-    title: 'Hello',
-    subTitle: 'World',
-  },
+  return data;
+}
 
-  {
-    id: '11',
-    title: 'Hello',
-    subTitle: 'World',
-  },
-  {
-    id: '12',
-    title: 'Hello',
-    subTitle: 'World',
-  },
-];
+const DATA = generateRandomData(20);
